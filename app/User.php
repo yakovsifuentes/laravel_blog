f2afee7f0677ba8f6ff1d17dcac44b362df31840
Rodrigo Sifuentes <yakov.sifuentes@gmail.com>
2019-07-21 19:03:49 -0500

    accessor para imagen y visualizacion en plantilla profile

--- app/User.php
+++ app/User.php
@@ -49,8 +49,8 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
-    public function updateProfile(Request $request)
+    public function getImageAttribute()
     {
-
+        return $this->profile_image;
     }
 }
